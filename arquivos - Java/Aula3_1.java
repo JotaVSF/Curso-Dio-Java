20595e69149e2ba6a5539df076e05d5e107cd299
João Vitor S Ferreira <joaovdesouzaf@gmail.com>
2025-06-27 17:54:47 -0300

    Add files via upload

---
+++ arquivos - Java/Aula3_1.java
@@ -0,0 +1,23 @@
+import domain.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Aula3_1 {
+    public static void main(String[] args) {
+        Map<String, User> users = new HashMap<>();
+        users.put("joao@joao.com", new User("João", 24));
+        users.put("chico@chico.com", new User("Chico", 26));
+        users.put("rani@rani.com", new User("Rani", 23));
+        users.put("clara@clara.com", new User("Clara", 25));
+        users.put("bruno@bruno.com", new User("Bruno", 25));
+        //System.out.print(users);
+
+        System.out.println("===========================\n");
+        users.keySet().forEach(System.out::println);
+        System.out.println("\n===========================\n");
+        users.values().forEach(System.out::println);
+
+
+    }
+}
